725bda697f539fa9feb631a4e582e9c076474af0
William Le <william7e.se@gmail.com>
2023-02-15 11:05:03 +0100

    Update Navigation.vue

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -18,15 +18,15 @@
                 <li class="inline-block underline mr-2 cursor-pointer">
                     <a :href="dinnerLink" target="_blank">Dinner</a>
                 </li>
+                <li class="inline-block underline mr-2 cursor-pointer">
+                    <a href="http://webshop.cong.se" target="_blank">Webshop</a>
+                </li>
                 <li class="inline-block underline">
                     <a
                         href="https://goo.gl/maps/khezqsjxGnsNzqx6A"
                         target="_blank"
                         >Find us</a
                     >
-                </li>
-                <li class="inline-block underline mr-2 cursor-pointer">
-                    <a href="http://webshop.cong.se" target="_blank">Webshop</a>
                 </li>
             </ul>
         </div>
