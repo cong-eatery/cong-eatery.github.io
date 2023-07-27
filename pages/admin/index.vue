8913ea4c04fbc84c2d614e561f56bfe456c1b3c2
therealmichaelvu <112770085+therealmichaelvu@users.noreply.github.com>
2023-07-27 02:13:54 +0200

    Update index.vue

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,6 +3,7 @@
         <AdminNavigation />
         <div class="sm:flex">
             <AdminEditAvailableSeats />
+            <AdminEditPDFMenu base-location="food/" title="LUNCH" />
             <AdminEditPDFMenu base-location="drink/" title="DINNER" />
         </div>
     </div>
